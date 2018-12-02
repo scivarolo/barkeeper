df3370d5ab17197c7004edd9bf39741c513ae403
Sebastian Civarolo <scivarolo@gmail.com>
2018-12-01 19:51:14 -0600

    Install react-router-dom, reactstrap, bootstrap, node-sass, and base theme customization

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import { Button } from 'reactstrap'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Hi
+          <h1>Barkeeper</h1>
+          <Button>Hi</Button>
         </header>
       </div>
     );
