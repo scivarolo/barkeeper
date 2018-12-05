1453d2d03344191d6d3e0eec231f01b381e10c1f
Sebastian Civarolo <scivarolo@gmail.com>
2018-12-05 15:31:24 -0600

    Buy items in shopping list and add to inventory

--- src/components/shoppinglist/ShoppingListItem.js
+++ src/components/shoppinglist/ShoppingListItem.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react'
 import {
   Button,
   ListGroupItem } from 'reactstrap'
+import API from '../../modules/data/API';
 
 class ShoppingListItem extends Component {
+
+  boughtItem = (item) => {
+    //console.log("bought item", item)
+    let userProductsObj = {
+      userId: item.userId,
+      productId: item.productId,
+      amountAvailable: item.product.fullAmount
+    }
+    // console.log("to inventory", userProductsObj)
+    //add item to userProducts
+    return API.saveData("userProducts", userProductsObj)
+      .then(() => this.props.deleteItem(item.id))
+
+    //remove item from userShopping
+    //using this.props.deleteItem
+  }
 
   render() {
     let item = this.props.item
     return (
-      <ListGroupItem className="mb-2" id={item.id}>
+      <ListGroupItem className="mb-2 d-flex" id={item.id}>
         <div>{item.product.name}</div>
-        <div>
+        <div className="ml-auto">
+          <Button className="btn-sm ml-2" onClick={() => this.boughtItem(item)}>Bought</Button>
           <Button className="btn-sm ml-2" onClick={() => this.props.deleteItem(item.id)}>Delete</Button>
         </div>
       </ListGroupItem>
