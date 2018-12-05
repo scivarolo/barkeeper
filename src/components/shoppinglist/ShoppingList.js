e2911f9e4117a92cf62c7bf75e5dd0f0560c8dda
Sebastian Civarolo <scivarolo@gmail.com>
2018-12-04 22:55:06 -0600

    basic shopping list output

--- src/components/shoppinglist/ShoppingList.js
+++ src/components/shoppinglist/ShoppingList.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
-
+import API from '../../modules/data/API'
 class ShoppingList extends Component {
 
-  state = {}
+  state = {
+    shoppingItems: [],
+    isLoaded: false
+  }
+
+  componentDidMount() {
+    return API.getWithExpand("shoppingItems", "item")
+    .then(items => {
+      this.setState({
+        shoppingItems: items,
+        isLoaded: true
+      })
+    })
+  }
 
   render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <h1>Shopping List</h1>
-          </Col>
-        </Row>
-      </Container>
-    )
+
+    let shoppingItems = this.state.shoppingItems
+
+    if (this.state.isLoaded) {
+      return (
+        <Container>
+          <Row>
+            <Col>
+              <h1>Shopping List</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <ul>
+                {
+                  shoppingItems.map(sItem => {
+                    return <li key={sItem.id}>{sItem.item.name}</li>
+                  })
+                }
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      )
+    } else {
+      return (
+        <Container>
+          <Row>
+            <Col>
+              <div>Loading</div>
+            </Col>
+          </Row>
+        </Container>
+      )
+    }
   }
 
 }
