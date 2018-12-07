22187aecd378c4e082d9340593e43cd8d230ab67
Sebastian Civarolo <scivarolo@gmail.com>
2018-12-07 14:58:24 -0600

    Add TODO

--- src/components/create/NewProduct.js
+++ src/components/create/NewProduct.js
@@ -13,6 +13,7 @@ import API from '../../modules/data/API';
 import user from '../../modules/data/user';
 
 //TODO: Allow new ingredient to be created when a new product is being created.
+//TODO: Give user feedback when a new product is created and added to their inventory.
 
 class NewProduct extends Component {
 
