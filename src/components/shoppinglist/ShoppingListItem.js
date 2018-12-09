4d42999067032b00f0e3fe65d3b99cec62ca830d
Sebastian Civarolo <scivarolo@gmail.com>
2018-12-08 23:49:58 -0600

    ability to add ingredients to shopping list

--- src/components/shoppinglist/ShoppingListItem.js
+++ src/components/shoppinglist/ShoppingListItem.js
@@ -7,22 +7,23 @@ import API from '../../modules/data/API';
 class ShoppingListItem extends Component {
 
   boughtItem = (item) => {
-
-    let userProductsObj = {
-      userId: item.userId,
-      productId: item.productId,
-      amountAvailable: item.product.fullAmount
+    if(item.productId) {
+      let userProductsObj = {
+        userId: item.userId,
+        productId: item.productId,
+        amountAvailable: item.product.fullAmount
+      }
+      //add item to userProducts and delete from userShopping
+      return API.saveData("userProducts", userProductsObj)
+        .then(() => this.props.deleteItem(item.id))
     }
-    //add item to userProducts and delete from userShopping
-    return API.saveData("userProducts", userProductsObj)
-      .then(() => this.props.deleteItem(item.id))
   }
 
   render() {
     let item = this.props.item
     return (
       <ListGroupItem className="mb-2 d-flex" id={item.id}>
-        <div>{item.product.name}</div>
+        <div>{item.productId ? `Product: ${item.product.name}` : `Ingredient: ${item.ingredient.label}`}</div>
         <div className="ml-auto">
           <Button className="btn-sm ml-2" onClick={() => this.boughtItem(item)}>Bought</Button>
           <Button className="btn-sm ml-2" onClick={() => this.props.deleteItem(item.id)}>Delete</Button>
--- src/modules/data/API.js
+++ src/modules/data/API.js
@@ -9,9 +9,8 @@ class API {
   }
 
   getAll(resource, userId) {
-    let url = `${this.baseUrl}/${resource}`
-    if (userId) url += `?userId=${userId}`
-
+    let url = `${this.baseUrl}/${resource}?id_ne=false`
+    if (userId) url += `&userId=${userId}`
     return fetch(url).then(r => r.json())
   }
 
@@ -20,13 +19,20 @@ class API {
   }
 
   getWithEmbed(resource, embed, userId) {
-    let url = `${this.baseUrl}/${resource}?_embed=${embed}`
+    let url = `${this.baseUrl}/${resource}?_embed=${embed}&id_ne=false`
+    if (userId) url += `&userId=${userId}`
+    return fetch(url).then(r => r.json())
+  }
+
+  getWithExpands(resource, userId, ...expands) {
+    let string = expands.reduce((string, expand) => `${string}&_expand=${expand}`, '')
+    let url = `${this.baseUrl}/${resource}?${string}&id_ne=false`
     if (userId) url += `&userId=${userId}`
     return fetch(url).then(r => r.json())
   }
 
   getWithExpand(resource, expand, userId) {
-    let url = `${this.baseUrl}/${resource}?_expand=${expand}`
+    let url = `${this.baseUrl}/${resource}?_expand=${expand}&id_ne=false`
     if (userId) url += `&userId=${userId}`
     return fetch(url).then(r => r.json())
   }
