fd8bea7a20fc17af82173a1950a69a07cb43f168
Sebastian Civarolo <scivarolo@gmail.com>
2018-12-27 15:48:14 -0600

    remove unused Dashboard import

--- src/components/Barkeeper.js
+++ src/components/Barkeeper.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import NavBar from "./NavBar"
 import { Switch, Route, Redirect } from "react-router-dom"
 
-import Dashboard from "./dashboard/Dashboard"
 import BarView from "./bar/BarView"
 import ShoppingListView from "./shoppinglist/ShoppingListView"
 import CocktailsView from './cocktails/CocktailsView'
