5cbe41b547ac443a94a9bbd37f7706457b50e6ba
Sebastian Civarolo <scivarolo@gmail.com>
2019-02-01 18:28:13 -0600

    convert login form to function component with hooks

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import {
   Button,
   Input,
@@ -7,38 +7,36 @@ import {
   FormGroup } from 'reactstrap'
 import "./login.scss"
 
-class LoginForm extends Component {
-  componentWillUnmount() {
-    this.props.resetFormState()
-  }
+function LoginForm(props) {
 
-  render() {
-    return (
-      <Form className="login-form" onSubmit={e => this.props.submitLogin(e)}>
-        <Button color="primary" onClick={e => this.props.submitLogin(e)}>AUTH</Button>
-        <h1 className="text-primary mb-3 font-weight-bold login-logo">Barkeeper</h1>
-        <h6 className="mb-3 mt-5 text-success">Please sign in</h6>
-        { this.props.registerSuccess
-          ? <p className="text-success">Your account was created successfully! Please log in.</p>
-          : null
-        }
-        { this.props.loginFailed
-          ? <p className="text-danger">Login failed. Please try again.</p>
-          : null
-        }
+  useEffect(() => {
+    props.resetFormState()
+  }, [])
 
-        <FormGroup>
-          <Label className="sr-only" for="loginUsername">Username</Label>
-          <Input type="text" id="loginUsername" placeholder="Username" required onChange={this.props.handleFieldChange} />
-          <Label className="sr-only" for="loginPassword">Password</Label>
-          <Input type="password" id="loginPassword" placeholder="Password" required onChange={this.props.handleFieldChange} />
-        </FormGroup>
+  return (
+    <Form className="login-form" onSubmit={e => props.submitLogin(e)}>
+      <h1 className="text-primary mb-3 font-weight-bold login-logo">Barkeeper</h1>
+      <h6 className="mb-3 mt-5 text-success">Please sign in</h6>
+      { props.registerSuccess
+        ? <p className="text-success">Your account was created successfully! Please log in.</p>
+        : null
+      }
+      { props.loginFailed
+        ? <p className="text-danger">Login failed. Please try again.</p>
+        : null
+      }
 
-        <Button color="primary" block size="lg" type="submit">Sign In</Button>
-        <p className="mt-2">Don&rsquo;t have an account? <span className="link" onClick={this.props.toggleForms}>Register</span></p>
-      </Form>
-    )
-  }
+      <FormGroup>
+        <Label className="sr-only" for="loginUsername">Username</Label>
+        <Input type="text" id="loginUsername" placeholder="Username" required onChange={e => props.handleFieldChange(e)} />
+        <Label className="sr-only" for="loginPassword">Password</Label>
+        <Input type="password" id="loginPassword" placeholder="Password" required onChange={e => props.handleFieldChange(e)} />
+      </FormGroup>
+
+      <Button color="primary" block size="lg" type="submit">Sign In</Button>
+      <p className="mt-2">Don&rsquo;t have an account? <span className="link" onClick={props.toggleForms}>Register</span></p>
+    </Form>
+  )
 
 }
 
